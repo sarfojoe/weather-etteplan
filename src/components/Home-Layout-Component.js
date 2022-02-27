e56abed8e53426f04172115f2a820f85fdf0afb5
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2022-02-27 11:57:09 +0200

    Header text corrected

--- src/components/Home-Layout-Component.js
+++ src/components/Home-Layout-Component.js
@@ -5,7 +5,7 @@ export function HomeLayout({ children }) {
   return (
     <ComponentContainer>
       <HeaderContainer>
-        <HeaderText>Säätutkä</HeaderText>
+        <HeaderText>Säätutka</HeaderText>
       </HeaderContainer>
       <BodyContainer>{children}</BodyContainer>
     </ComponentContainer>
